f7c416997fe04a876119f779e5d8cc974ca98518
Gaurav Kumar <gauravpal7710@gmail.com>
2018-08-02 02:46:26 -0700

    first

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class Solution {
+	public static void main(String[] args) {
+   Scanner sc= new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(s);
+    }
+}
